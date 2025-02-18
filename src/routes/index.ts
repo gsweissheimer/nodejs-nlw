935bd654ab1a5c7e4b8b024bc08cc59a0446fb49
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-02-18 19:43:13 -0300

    Setup initial API structure with TypeScript, Swagger and Biome.

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { subscribeToEventRoute } from './subscribe-to-event-route'
+
+export {
+    subscribeToEventRoute
+}
---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,25 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from "zod";
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+    app.post("/subscriptions", {
+        schema: {
+            summary: "Subscribe to an event",
+            description: "Subscribe to an event by providing your name and email",
+            tags: ["subscriptions"],
+            body: z.object({
+                name: z.string().min(2),
+                email: z.string().email(),
+            }),
+            response: {
+                201: z.object({
+                    name: z.string()
+                }),
+            },
+        },
+    }, async (request, reply) => {
+        const { name } = request.body;
+    
+        return reply.status(201).send({ name });
+    });
+}
