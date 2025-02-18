46e1dba54559f5fae7b0b2c6ac89f582e32d8ea8
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-02-18 20:17:16 -0300

    Setup data storege configuration.

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import {z} from "zod";
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3001),
+  POSTGRESQL_URL: z.string().url().default("postgres://postgres:password@localhost:5433/postgres"),
+  REDIS_URL: z.string().url().default("redis://localhost:6379"),
 });
 
 export const env = envSchema.parse(process.env);
