6775a83a22643aafe2f29ad9215a672ae3d5a095
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-04-12 18:37:14 -0300

    feat: Add initial log statement for login route execution

--- src/routes/registration/login-route.ts
+++ src/routes/registration/login-route.ts
@@ -10,6 +10,7 @@ export interface Response {
 }
 
 export const loginRoute: FastifyPluginAsyncZod = async app => {
+  console.log('*** INÍCIO: Executando plugin loginRoute MÍNIMO ***')
     app.post(
       '/login',
       {
