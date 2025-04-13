911726158dc2f471721fc7ba5db1dc252f38db57
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-04-13 08:26:49 -0300

    fix: Update database URL in drizzle.config.ts to use hardcoded connection string

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from 'drizzle-kit'
-import { env } from './src/env';
 
 export default {
     schema: './src/drizzle/schema/*',
     out: './src/drizzle/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        url: env.POSTGRESQL_URL
+        url: 'postgresql://docker:docker@localhost:5433/connect'
     }
 } satisfies Config;
