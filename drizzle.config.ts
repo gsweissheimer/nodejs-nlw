38dbb4003e2ebea6ddb412a7e7d4ed5dc0825dbf
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-04-13 08:56:43 -0300

    fix: Update dependencies in package.json and package-lock.json for compatibility

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
-export default {
+export default defineConfig({
     schema: './src/drizzle/schema/*',
     out: './src/drizzle/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://docker:docker@localhost:5433/connect'
-    }
-} satisfies Config;
+            url: 'postgresql://docker:docker@localhost:5433/connect'
+        }
+    });
